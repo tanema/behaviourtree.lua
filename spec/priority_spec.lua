2bca93755808497529f4b015bc2eb95b9f7ea2b8
Tim Anema <timanema@gmail.com>
2014-09-18 22:58:36 -0400

    almost implemented all of the tests

---
+++ spec/priority_spec.lua
@@ -0,0 +1,158 @@
+require 'spec/custom_asserts'
+local BehaviourTree = require 'behaviour_tree'
+
+describe('Priority selector', function()
+  local selector
+  describe('can be constructed with', function()
+    describe("an object containing it's title", function()
+      before_each(function()
+        selector = BehaviourTree.Priority:new({ title = 'choose' })
+      end)
+
+      it('and the title is saved on the instance', function()
+        assert.are.equal(selector.title, 'choose')
+      end)
+    end)
+  end)
+
+  describe('instance', function()
+    before_each(function()
+      selector = BehaviourTree.Priority:new({ title = 'selector' })
+    end)
+
+    it('has the same methods like a BranchNode instance', function()
+      assert.is_function(selector._run)
+      assert.is_function(selector.run)
+      assert.is_function(selector.start)
+      assert.is_function(selector.finish)
+      assert.is_function(selector.running)
+      assert.is_function(selector.success)
+      assert.is_function(selector.fail)
+    end)
+  end)
+
+  describe('when having a Priority with two nodes', function()
+    local node, hasRun1, beSuccess1, startCalled1, endCalled1,
+        hasRun2, startCalled2, endCalled2
+    before_each(function()
+      beSuccess1 = true
+      startCalled1 = 0
+      startCalled2 = 0
+      hasRun1 = false
+      endCalled1 = false
+      hasRun2 = false
+      endCalled2 = false
+      node = BehaviourTree.Node:new({
+        run = function(self)
+          hasRun1 = true
+          if beSuccess1 then
+            self:success()
+          else
+            self:fail()
+          end
+        end,
+        start = function() startCalled1 = startCalled1 + 1 end,
+        finish = function() endCalled1 = true end
+      })
+      selector = BehaviourTree.Priority:new({
+        title = 'a selector',
+        nodes = {
+          node,
+          BehaviourTree.Node:new({
+            run = function(self) 
+              hasRun2 = true 
+              self:success()
+            end,
+            start = function() startCalled2 = startCalled2 + 1 end,
+            finish = function() endCalled2 = true end
+          })
+        }
+      })
+      selector:setControl({
+        success = function() end,
+        fail = function() end
+      })
+    end)
+
+    describe('if success is called by first task', function()
+      before_each(function()
+        beSuccess1 = true
+        selector:run()
+      end)
+
+      it("does not call start of the next task in line", function()
+        assert.are.equal(startCalled2, 0)
+      end)
+
+      it("does not call run of the next task in line", function()
+        assert.falsy(hasRun2)
+      end)
+    end)
+
+    describe('if fail is called by first task', function()
+      before_each(function()
+        beSuccess1 = false
+        selector:run()
+      end)
+
+      it("calls start of the next task in line", function()
+        assert.are.equal(startCalled2, 1)
+      end)
+
+      it("calls run of the next task in line", function()
+        assert.truthy(hasRun2)
+      end)
+    end)
+  end)
+
+  describe('when in Priority with two nodes', function()
+    local node, parentSelector, beSuccess, parentSuccessCalled, parentFailCalled
+    before_each(function()
+      beSuccess = true
+      parentSuccessCalled = false
+      parentFailCalled = false
+      selector = BehaviourTree.Priority:new({
+        title = 'a selector',
+        nodes = {
+          BehaviourTree.Node:new({
+            run = function(self) 
+              if beSuccess then 
+                self:success() 
+              else 
+                self:fail()
+              end
+            end
+          })
+        }
+      })
+
+      parentSelector = BehaviourTree.Priority:new({
+        title = 'parent',
+        nodes = { selector },
+        success = function()
+          parentSuccessCalled = true
+        end,
+        fail = function()
+          parentFailCalled = true
+        end
+      })
+    end)
+
+    describe('one task results in success', function()
+      it('calls success also in parent node', function()
+        beSuccess = true
+        parentSelector:run()
+        assert.truthy(parentSuccessCalled)
+      end)
+    end)
+
+    describe('all task result in failure', function()
+      it('calls fail also in parent node', function()
+        beSuccess = false
+        parentSelector:run()
+        assert.truthy(parentFailCalled)
+      end)
+    end)
+  end)
+end)
+
---
+++ spec/random_spec.lua
@@ -0,0 +1,234 @@
+require 'spec/custom_asserts'
+local BehaviourTree = require 'behaviour_tree'
+
+describe('Random', function()
+  local random
+  describe('can be constructed with', function()
+    describe('an object containing it\'s title', function()
+      before_each(function()
+        random = BehaviourTree.Random:new({ title = 'runAny' })
+      end)
+
+      it('and the title is saved on the instance', function()
+        assert.are.equal(random.title, 'runAny')
+      end)
+    end)
+  end)
+
+  describe('instance', function()
+    before_each(function()
+      random = BehaviourTree.Random:new({ title = 'random' })
+    end)
+
+    it('has the same methods like a BranchNode instance', function()
+      assert.is_function(random._run)
+      assert.is_function(random.run)
+      assert.is_function(random.start)
+      assert.is_function(random.finish)
+      assert.is_function(random.running)
+      assert.is_function(random.success)
+      assert.is_function(random.fail)
+    end)
+  end)
+
+  describe('when having a RandomSelector with two nodes', function()
+    local node1, node2, hasRun1, beSuccess, willRun, startCalled1, endCalled1,
+        hasRun2, startCalled2, endCalled2, selector
+
+    before_each(function()
+      beSuccess = true
+      hasRun1 = 0
+      hasRun2 = 0
+      startCalled1 = false
+      endCalled1 = false
+      startCalled2 = false
+      endCalled2 = false
+      node1 = BehaviourTree.Node:new({
+        run = function(self)
+          hasRun1 = hasRun1 + 1
+          if willRun then
+            self:running()
+          elseif beSuccess then
+            self:success()
+          else 
+            self:fail()
+          end
+        end,
+        start = function() startCalled1 = true end,
+        finish = function() endCalled1 = true end
+      })
+      node2 = BehaviourTree.Node:new({
+        run = function(self)
+          hasRun2 = hasRun2 + 1
+          if willRun then
+            self:running()
+          elseif beSuccess then
+            self:success()
+          else
+            self:fail()
+          end
+        end,
+        start = function() startCalled2 = true end,
+        finish = function() endCalled2 = true end
+      })
+      selector = BehaviourTree.Random:new({
+        title = 'a random',
+        nodes = {
+          node1,
+          node2
+        }
+      })
+      selector:setControl({
+        running = function() end,
+        success = function() end,
+        fail = function() end
+      })
+    end)
+
+    describe('if task will be success', function()
+      local old_rand = math.random
+      before_each(function()
+        beSuccess = true
+        math.random = function() return 0.7 end
+        selector:run()
+      end)
+
+      after_each(function()
+        math.random = old_rand
+      end)
+
+      it('runs randomly one task', function()
+        assert.are.equal(hasRun1, 0)
+        assert.truthy(hasRun2)
+      end)
+
+      it('starts randomly only one task', function()
+        assert.falsy(startCalled1)
+        assert.truthy(startCalled2)
+      end)
+    end)
+
+    describe('if task will fail', function()
+      local old_rand = math.random
+      before_each(function()
+        beSuccess = false
+        math.random = function() return 0.7 end
+        selector:run()
+      end)
+
+      after_each(function()
+        math.random = old_rand
+      end)
+
+      it('runs randomly only that one task', function()
+        assert.are.equal(hasRun1, 0)
+        assert.truthy(hasRun2)
+      end)
+
+      it('starts randomly only that one task', function()
+        assert.falsy(startCalled1)
+        assert.truthy(startCalled2)
+      end)
+    end)
+
+    describe('if task will return running', function()
+      local old_rand = math.random
+      before_each(function()
+        willRun = true
+        math.random = function() return 0.7 end
+        selector:run()
+      end)
+
+      after_each(function()
+        math.random = old_rand
+      end)
+
+      it('will run that one again on run() and only start it once', function()
+        assert.are.equal(hasRun1, 0)
+        assert.are.equal(hasRun2, 1)
+        assert.truthy(startCalled2)
+
+        startCalled2 = false
+        math.random = function() return 0.3 end
+        selector:run()
+        assert.are.equal(hasRun1, 0)
+        assert.are.equal(hasRun2, 2)
+        assert.falsy(startCalled2)
+
+        math.random = function() return 0.1 end
+        selector:run()
+        assert.are.equal(hasRun1, 0)
+        assert.are.equal(hasRun2, 3)
+        assert.falsy(startCalled2)
+      end)
+
+      it('starts randomly only that one task', function()
+        assert.falsy(startCalled1)
+        assert.truthy(startCalled2)
+      end)
+    end)
+  end)
+
+  describe('when in RandomSelector with two nodes', function()
+    local parentSelector, beSuccess, willRun, parentSuccessCalled, parentFailCalled, parentRunningCalled, selector
+    before_each(function()
+      beSuccess = true
+      parentSuccessCalled = false
+      parentFailCalled = false
+      selector = BehaviourTree.Random:new({
+        title = 'a random',
+        nodes = {
+          BehaviourTree.Node:new({
+            run = function(self)
+              if willRun then
+                self:running()
+              elseif beSuccess then
+                self:success()
+              else
+                self:fail()
+              end
+            end
+          })
+        }
+      })
+
+      parentSelector = BehaviourTree.Random:new({
+        title = 'parent',
+        nodes = { selector },
+        running = function()
+          parentRunningCalled = true
+        end,
+        success = function()
+          parentSuccessCalled = true
+        end,
+        fail = function()
+          parentFailCalled = true
+        end
+      })
+    end)
+
+    describe('all task result in success', function()
+      it('calls success also in parent node', function()
+        beSuccess = true
+        parentSelector:run()
+        assert.truthy(parentSuccessCalled)
+      end)
+    end)
+
+    describe('a task results in failure', function()
+      it('calls fail also in parent node', function()
+        beSuccess = false
+        parentSelector:run()
+        assert.truthy(parentFailCalled)
+      end)
+    end)
+
+    describe('a task resulting in running', function()
+      it('calls running also in parent node', function()
+        willRun = true
+        parentSelector:run()
+        assert.truthy(parentRunningCalled)
+      end)
+    end)
+  end)
+end)
